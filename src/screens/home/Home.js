bd2ea11d603ddd4fcb1ce53c7fa39c0eadab76b5
Sn3haMittal <snehamittal071@gmail.com>
2022-11-29 01:41:00 +1100

    Adding Home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,22 +1,154 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from '../../common/header/Header';
+import React, { useEffect, useState } from "react";
+import Header from "../../common/header/Header";
+import './Home.css';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import FilterCard from "./FilterCard";
 
-export default function Home() {
+const Home = () => {
 
+    const [filterParam, setFilterParam] = useState({ 
+        'title': '',
+        'genres': '',
+        'artists':'',
+        'startDate': '',
+        'endDate':''
+    });
 
     return (
-        <div className="home">
-            <h1>This is Home page</h1>
+        <div>
+            <Header />
+            <div id='upcoming-movies-heading-container'>
+                <div id='upcoming-movies-heading'>
+                    Upcoming Movies
+                </div>
+            </div>
+            <div id='upcoming-movies'>
+                <UpcomingMovies />
+            </div>
+            <div id="released-movies-and-filter-container">
+                <div className="released-movies grid-76 margin-16">
+                    <ReleasedMovies filterParam={filterParam} />
+                </div>
+                <div className="movie-filter grid-24 margin-16">
+                    <FilterCard setFilterParam={setFilterParam} />
+                </div>
+            </div>
         </div>
     )
-
-    // return(
-    //     <Router>
-    //         <div>
-    //             <Route exact path='/' render={ () => <Header />} />
-    //         </div>
-            
-    //     </Router>)
-    
 }
+
+export function UpcomingMovies() {
+    const [upcomingMovies, setUpcomingMovies] = useState([])
+
+    useEffect(() => {
+        getUpcomingMovies()
+    }, [])
+
+    // Make an api call for get published/upcoming movies
+    const getUpcomingMovies = async () => {
+        try {
+            const status = 'PUBLISHED'; // Published == Upcoming
+            // Will return 6 upcoming movies as I set limit to 6 (as per requirement)
+            const rawResponse = await fetch(`http://localhost:8085/api/v1/movies?status=${status}&limit=6`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json;charset=UTF-8",
+                    "Accept": "application/json;charset=UTF-8",
+                },
+            })
+
+            const response = await rawResponse.json()
+
+            if (rawResponse.ok) {
+                setUpcomingMovies(response.movies);
+            } else {
+                throw (new Error(response.message || 'Something went wrong!'))
+            }
+        } catch (e) {
+            alert(`Error: ${e.message}`);
+        }
+    }
+
+    return (
+        <ImageList
+            rowHeight={250}
+            sx={{
+                gridAutoFlow: "column",
+                gridTemplateColumns: "repeat(auto-fill,minmax(250px,1fr)) !important",
+                gridAutoColumns: "minmax(250px, 1fr)",
+                margin: 'auto',
+                overflowY: 'hidden'
+            }}>
+            {
+                upcomingMovies.map((movie) => (
+                    <ImageListItem key={movie.id}>
+                        <img src={movie.poster_url} alt={movie.title} />
+                        <ImageListItemBar title={movie.title} />
+                    </ImageListItem>
+                ))
+            }
+        </ImageList>
+    )
+}
+
+
+export function ReleasedMovies(props) {
+
+    const [releasedMovies, setReleasedMovies] = useState([])
+
+    const { title, genres, artists, startDate, endDate } = props.filterParam;
+
+    useEffect(() => {
+        getReleasedMovies()
+    }, [props.filterParam])
+
+    // Make an api call for get released movies
+    const getReleasedMovies = async () => {
+        try {
+            const status = 'RELEASED';
+            const url = `http://localhost:8085/api/v1/movies?status=${status}&title=${title}&start_date=${startDate}&end_date=${endDate}&genre=${genres}&artists=${artists}`
+
+            const rawResponse = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json;charset=UTF-8",
+                    "Accept": "application/json;charset=UTF-8",
+                },
+            })
+
+            const response = await rawResponse.json()
+
+            if (rawResponse.ok) {
+                setReleasedMovies(response.movies);
+            } else {
+                throw (new Error(response.message || 'Something went wrong!'))
+            }
+        } catch (e) {
+            alert(`Error: ${e.message}`);
+        }
+    }
+
+    return (
+        <ImageList
+            rowHeight={350}
+            cols={4}
+            sx={{ overflow: 'hidden', margin: 'auto' }}
+            gap={10}
+        >
+            {
+                releasedMovies.map((movie) => (
+                    <ImageListItem key={movie.id} className='back'>
+                        <img src={movie.poster_url} alt={movie.title} />
+                        <ImageListItemBar title={movie.title} subtitle={`Released Date : ${movie.release_date}`} />
+                    </ImageListItem>
+                ))
+            }
+        </ImageList>
+    )
+}
+
+
+
+export default Home;
